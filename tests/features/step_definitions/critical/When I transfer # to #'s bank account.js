e5c30ea3ab489859c195532dca694f905261eb67
lgandecki <lgandecki@css.edu>
2016-02-25 12:38:21 +0100

    try without having duplicated steps

--- tests/features/step_definitions/critical/When I transfer # to #'s bank account.js
+++ tests/features/step_definitions/critical/When I transfer # to #'s bank account.js
@@ -1,6 +1,6 @@
-module.exports = function () {
-  this.When(/^I transfer \$(\d+) to "([^"]*)"'s bank account$/, function (amount, otherAccountHolderName) {
-    var otherAccountHolderNumber = this.accounts[otherAccountHolderName]._id;
-    widgets.bankTransfer.transferAmountToAccountNumber(amount, otherAccountHolderNumber);
-  });
-};
+//module.exports = function () {
+//  this.When(/^I transfer \$(\d+) to "([^"]*)"'s bank account$/, function (amount, otherAccountHolderName) {
+//    var otherAccountHolderNumber = this.accounts[otherAccountHolderName]._id;
+//    widgets.bankTransfer.transferAmountToAccountNumber(amount, otherAccountHolderNumber);
+//  });
+//};
